e8bc8dbd581bb7c1bd262cb8dcd8f447d2381841
summerli2028 <summerli@stanford.edu>
2024-07-08 01:29:45 +0000

    [LeetCode Sync] Runtime - 1 ms (90.32%), Memory - 45.7 MB (45.83%)

---
+++ submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for(int i =0; i<prices.length-1; i++){
+            if(prices[i+1]>prices[i]){
+                sum-=prices[i];
+                sum+=prices[i+1];
+            }
+        }
+        return sum;
+            
+
+
+
+            }
+        }
+        
+    
